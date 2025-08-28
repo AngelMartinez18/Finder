f7bfdd1ae45bc34546af4c114edc60558b71b907
AngelMartinez18 <marangelantonio18@gmail.com>
2025-08-28 16:30:15 -0600

    Cambios minimos, integracion de tailwindcss, rutas y algunas vistas en blanco

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+    // tailwind.config.js
+    export default {
+      content: [
+        './resources/views/**/*.blade.php',
+        './resources/js/**/*.{js,vue,jsx,ts,tsx}',
+      ],
+      theme: {
+        extend: {},
+      },
+      plugins: [],
+    }
